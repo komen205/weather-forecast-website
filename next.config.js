d2fd94cc554715f49a21e80e08cb447b67ebb044
komen205 <59452877+komen205@users.noreply.github.com>
2021-03-20 19:19:57 +0000

    setting up Pwa

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+  future: { webpack5: true },
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+});
--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,21 @@ export default class CustomDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#317EFB" />
+          <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         </Head>
         <body>
           <video loop autoPlay muted id="videoBackground">
